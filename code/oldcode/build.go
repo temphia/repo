b20ef624ea014a6d8fdf3eee80e8ded558e767b1
bornjre <bornjre@gmail.com>
2023-05-01 20:22:51 +0545

    move old files

--- code/oldcode/build.go
+++ code/oldcode/build.go
@@ -16,6 +16,29 @@ import (
 	"github.com/tidwall/gjson"
 )
 
+// fout, err := os.ReadFile("repo.json")
+// handleErr(err)
+
+// rb, err := code.New(fout)
+// handleErr(err)
+
+// err = rb.BuildAll()
+// handleErr(err)
+
+// fmt.Printf("Out of %d, %d built sucessfully and %d errored out \n", len(rb.Config.Items), len(rb.Outputs), len(rb.ErroredItems))
+
+// for k, err2 := range rb.ErroredItems {
+// 	fmt.Printf(" [ %s ] errored |> %+v ", k, err2)
+// 	fmt.Println(err2.Error())
+// }
+
+// for k, outFolder := range rb.Outputs {
+// 	fmt.Printf(" [ %s ] output |> %+v ", k, outFolder)
+
+// }
+
+// pp.Println(rb.IndexAll(false))
+
 func (rb *RepoBuild) buildItem(name string) (string, error) {
 
 	fmt.Println("Building ", name)
--- main.go
+++ main.go
@@ -1,37 +1,11 @@
 package main
 
-import (
-	"fmt"
-	"os"
-
-	"github.com/k0kubun/pp"
-	"github.com/temphia/repo/code"
-)
+import "github.com/k0kubun/pp"
 
 func main() {
 
-	fout, err := os.ReadFile("repo.json")
-	handleErr(err)
+	pp.Println()
 
-	rb, err := code.New(fout)
-	handleErr(err)
-
-	err = rb.BuildAll()
-	handleErr(err)
-
-	fmt.Printf("Out of %d, %d built sucessfully and %d errored out \n", len(rb.Config.Items), len(rb.Outputs), len(rb.ErroredItems))
-
-	for k, err2 := range rb.ErroredItems {
-		fmt.Printf(" [ %s ] errored |> %+v ", k, err2)
-		fmt.Println(err2.Error())
-	}
-
-	for k, outFolder := range rb.Outputs {
-		fmt.Printf(" [ %s ] output |> %+v ", k, outFolder)
-
-	}
-
-	pp.Println(rb.IndexAll(false))
 }
 
 func handleErr(err error) {
