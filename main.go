632791aeb94a4e00b8d10cf25c5739562df15b3a
bornjre <bornjre@gmail.com>
2023-05-02 19:22:28 +0545

    log error

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"log"
+
 	"github.com/k0kubun/pp"
 	rcmd "github.com/temphia/temphia/code/tools/repobuild/cmd"
 )
 
 func main() {
-
-	pp.Println(rcmd.Run(".repo.yaml"))
-
+	err := rcmd.Run(".repo.yaml")
+	if err != nil {
+		pp.Println(err)
+		log.Println(err.Error())
+	}
 }
