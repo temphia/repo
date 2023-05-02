e4d873c1104439e95b2d5c5d515309f857141698
bornjre <bornjre@gmail.com>
2023-05-02 20:07:29 +0545

    create build folder

--- main.go
+++ main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
-	"log"
-
 	"github.com/k0kubun/pp"
+	"github.com/temphia/temphia/code/backend/libx/xutils"
 	rcmd "github.com/temphia/temphia/code/tools/repobuild/cmd"
 )
 
 func main() {
-	err := rcmd.Run(".repo.yaml")
+
+	err := xutils.CreateIfNotExits("build")
+	if err != nil {
+		panic(err)
+	}
+
+	pp.Println()
+
+	err = rcmd.Run(".repo.yaml")
 	if err != nil {
 		pp.Println(err)
-		log.Println(err.Error())
+		panic(err.Error())
 	}
 }
