88aa8dfe8808884960b929a85691ed7a124d6447
bornjre <bornjre@gmail.com>
2023-05-02 20:32:34 +0545

    update imports for new debugs logs

--- main.go
+++ main.go
@@ -2,20 +2,11 @@ package main
 
 import (
 	"github.com/k0kubun/pp"
-	"github.com/temphia/temphia/code/backend/libx/xutils"
 	rcmd "github.com/temphia/temphia/code/tools/repobuild/cmd"
 )
 
 func main() {
-
-	err := xutils.CreateIfNotExits("build")
-	if err != nil {
-		panic(err)
-	}
-
-	pp.Println()
-
-	err = rcmd.Run(".repo.yaml")
+	err := rcmd.Run(".repo.yaml")
 	if err != nil {
 		pp.Println(err)
 		panic(err.Error())
